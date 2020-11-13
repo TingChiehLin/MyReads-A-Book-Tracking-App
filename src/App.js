89041d736ad5efc0b025d8a0da04c38ff9cb7f5e
Jay Lin <cooloojayoo@gmail.com>
2020-11-14 01:06:29 +1100

    fix category

--- src/App.js
+++ src/App.js
@@ -48,14 +48,14 @@ class BooksApp extends React.Component {
                     <Book 
                       bookTitle = "To Kill a Mockingbird" 
                       bookAuthor = "Harper Lee"
-                      category = "Currently Reading"
+                      category = {this.state.category}
                       imageUrl = "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
                     />
 
                     <Book 
                       bookTitle = "To Kill a Mockingbird" 
                       bookAuthor = "Harper Lee"
-                      category = "Currently Reading" 
+                      category = {this.state.category}
                       imageUrl = "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
                     />
                 </ol>
@@ -68,7 +68,7 @@ class BooksApp extends React.Component {
                     <Book 
                       bookTitle = "To Kill a Mockingbird" 
                       bookAuthor = "Harper Lee"
-                      category = "Currently Reading" 
+                      category = {this.state.category}
                       imageUrl = "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
                     />
                 </ol>
